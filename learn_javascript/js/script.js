c178672b11d703369582cd163ffd0d8806d70355
gio1996 <farid@polargold.de>
2014-01-28 21:09:31 +0100

    Bold the menu

--- learn_javascript/js/script.js
+++ learn_javascript/js/script.js
@@ -7,3 +7,11 @@ $("article h1").on("mouseenter mouseleave", function () {
 
   $("article h1").off();
 });
+
+$(".main-nav a").on("click", function () {
+	var linkWasBold = $(this).hasClass("bold");
+	$("a").removeClass("bold");
+	if(linkWasBold == false) {
+		$(this).addClass("bold");
+	}
+});
