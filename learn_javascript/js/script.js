7305783831bbf92e1ee281254b3c7795545339a4
gio1996 <farid@polargold.de>
2014-02-11 21:29:40 +0100

    Refactor javascript and add data type comment

--- learn_javascript/js/script.js
+++ learn_javascript/js/script.js
@@ -1,10 +1,55 @@
 
-function toggleArticle() {
-  $(this).toggleClass("blue");
-  $("p").toggleClass("hidden");
-  $("footer").toggleClass("increase-font-size");
+/* data types
+
+String
+"hello world";
+
+Boolean
+true;
+
+Function
+function optionalName (optionalParameter) {
+	// inside the function
+}
+
+Object
+{
+	myKey: "any value",
+	anotherKey: true
+}
+*/
+
+
+
+
+
+function toggleArticle () {
+	var myH1 = this;
+	$("p").fadeIn({
+		duration: 800,
+		complete: function () {
+			$(myH1).addClass("blue");
+		}
+	});
+
+	$("footer").toggleClass("increase-font-size");
 }
 $("article h1").on("click", toggleArticle);
+
+var caja = {
+	legs: 4,
+	nose: 1,
+	color: "white",
+	isDog: true,
+	bark: function () {
+		console.log("wuff");
+	}
+};
+
+function hasClass (name) {
+ // logic
+ return false;
+}
 
 
 function boldMenu() {
